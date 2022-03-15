e01c17bec406e9cd8aabc22be89d03712cbc8033
ykskks <fkt.kohei@gmail.com>
2022-03-15 23:05:50 +0900

    use pathlib.Path for os compatibility

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,34 +4,40 @@ from pathlib import Path
 
 from lightpype import Pipeline, Script
 
+SRCDIR_NAME = "src"
 LOGDIR_NAME = ".lightpype"
 LOGFILE_NAME = "log.json"
 
 
-def _make_sample_script(srcdir, filename: str):  # -> py.path.local
-    script = srcdir.join(f"{filename}.py")
-    script.write(f"print('{filename}')")
+def _make_sample_script(tmpdir: Path, filename: str) -> Path:
+    srcdir = tmpdir / "src"
+    srcdir.mkdir(exist_ok=True)
+
+    script = srcdir / f"{filename}.py"
+    script.write_text(f"print('{filename}')")
     return script
 
 
-def _make_sample_logfile(tmpdir):  # -> py.path.local
-    logfile = tmpdir.mkdir(LOGDIR_NAME).join(LOGFILE_NAME)
-    logfile.write('{"last_executed_at": ' + f'"{str(datetime.now())}"' + "}")
+def _make_sample_logfile(tmpdir: Path):
+    logfile = tmpdir / LOGDIR_NAME / LOGFILE_NAME
+    logfile.parent.mkdir(parents=True, exist_ok=True)
+
+    logfile.write_text('{"last_executed_at": ' + f'"{str(datetime.now())}"' + "}")
 
 
 class TestPipeline:
     def test_first_run(self, tmpdir, capfd):
-        srcdir = tmpdir.mkdir("src")
+        tmpdir = Path(tmpdir)
 
-        first_py = _make_sample_script(srcdir, "first")
-        second_py = _make_sample_script(srcdir, "second")
+        first_py = _make_sample_script(tmpdir, "first")
+        second_py = _make_sample_script(tmpdir, "second")
 
         scripts = [
-            Script(Path(first_py)),
-            Script(Path(second_py)),
+            Script(first_py),
+            Script(second_py),
         ]
 
-        pipeline = Pipeline(scripts, Path(tmpdir))
+        pipeline = Pipeline(scripts, tmpdir)
         pipeline.run()
 
         # use capfd so that output from subprocesses are captured
@@ -39,19 +45,19 @@ class TestPipeline:
         assert captured.out == "first\nsecond\n"
 
     def test_no_execution(self, tmpdir, capfd):
-        srcdir = tmpdir.mkdir("src")
+        tmpdir = Path(tmpdir)
 
-        first_py = _make_sample_script(srcdir, "first")
-        second_py = _make_sample_script(srcdir, "second")
+        first_py = _make_sample_script(tmpdir, "first")
+        second_py = _make_sample_script(tmpdir, "second")
 
         _make_sample_logfile(tmpdir)
 
         scripts = [
-            Script(Path(first_py)),
-            Script(Path(second_py)),
+            Script(first_py),
+            Script(second_py),
         ]
 
-        pipeline = Pipeline(scripts, Path(tmpdir))
+        pipeline = Pipeline(scripts, tmpdir)
         pipeline.run()
 
         # use capfd so that output from subprocesses are captured
@@ -59,21 +65,21 @@ class TestPipeline:
         assert captured.out == ""
 
     def test_only_second(self, tmpdir, capfd):
-        srcdir = tmpdir.mkdir("src")
+        tmpdir = Path(tmpdir)
 
-        first_py = _make_sample_script(srcdir, "first")
+        first_py = _make_sample_script(tmpdir, "first")
 
         _make_sample_logfile(tmpdir)
         time.sleep(1.0)  # make sure the logfile is written first
 
-        second_py = _make_sample_script(srcdir, "second")
+        second_py = _make_sample_script(tmpdir, "second")
 
         scripts = [
-            Script(Path(first_py)),
-            Script(Path(second_py)),
+            Script(first_py),
+            Script(second_py),
         ]
 
-        pipeline = Pipeline(scripts, Path(tmpdir))
+        pipeline = Pipeline(scripts, tmpdir)
         pipeline.run()
 
         # use capfd so that output from subprocesses are captured
